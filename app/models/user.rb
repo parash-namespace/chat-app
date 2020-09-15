b8df4c9a1be98438e64afe5141f6c582bd4bfed3
parash <parash.a@namespace.jp>
2020-09-15 20:08:02 +0545

    completed but need to check errors in recent conversation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   has_one_attached :photo
+  has_many :conversations
 
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
